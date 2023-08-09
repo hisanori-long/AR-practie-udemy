0c348211eec58d73567d5f996e98a49ade42839b
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2023-08-09 12:08:17 +0900

    Day2 Learned about script

--- Assets/Scripts/RotateSphere.cs
+++ Assets/Scripts/RotateSphere.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class RotateSphere : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector3 movement;
+    public int xi, yi, zi;
     void Start()
     {
-        
+        movement = new Vector3(xi, yi, zi);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(movement * Time.deltaTime);
     }
 }
